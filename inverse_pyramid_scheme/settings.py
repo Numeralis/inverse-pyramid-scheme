be00590100483283956f2b09e4909071f0228390
Meinhard Christian <gdmeinhard@gmail.com>
2024-09-25 06:29:34 +0700

    Tugas 4 Finished

--- inverse_pyramid_scheme/settings.py
+++ inverse_pyramid_scheme/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-of*=s%kr@lly2*ab(hqoh!_pn3++h(ywr3b4dlqhydp+%l8_bj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "meinhard-christian-inversepyramidscheme.pbp.cs.ui.ac.id"]
 
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'inverse_pyramid_scheme.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://meinhard-christian-inversepyramidscheme.pbp.cs.ui.ac.id", "https://meinhard-christian-inversepyramidscheme.pbp.cs.ui.ac.id"]
